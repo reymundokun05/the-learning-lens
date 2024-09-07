8e518bf306ebf869547d25359c18a7aa9298fb6f
reymundokun05 <reymundo052411@gmail.com>
2024-09-08 02:12:06 +0800

    Add files via upload

---
+++ js/loadnav.js
@@ -0,0 +1,15 @@
+function loadNav() {
+	const navbarcont = `<header class="headerbar">
+	  <img src="images/logo/logo.png" style="margin-left:30px;width:150px;height:60px;float:left" />
+	  <span style="float:left;padding:20px 0;"><b>&nbsp;&nbsp;&nbsp;The Learning Lens</b></span>
+	  <ul class="links">
+	    <li><a href="reflections.html"><span class="fa fa-book"></span>&nbsp;Reflections/Journal Entries</a></li>
+	    <li><a href="index.html"><span class="fa fa-home"></span>&nbsp;Home</a></li>
+	  </ul>
+	</header>
+	`;
+	
+	const footercont = `<footer style='position: fixed;height: 20px;background: #6067ba;color: white;bottom: 0;left: 0;right: 0px;width: 100%;margin-bottom: 0px;clear: both;text-align: center;padding: 15px;'>(c) 2024 Reymundo Esteves II</footer>`;
+	document.getElementById('headerbar').insertAdjacentHTML('afterbegin', navbarcont);
+	document.getElementById('footer').insertAdjacentHTML('afterbegin', footercont);
+}
